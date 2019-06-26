cc01f39297740337c1950de3ffe040d968a9a89e
Steve Ryan <steveryan6989@gmail.com>
2019-06-26 15:59:25 -0400

    Adds a homepage with explanations of, and links to, the different forms.

--- config/routes.rb
+++ config/routes.rb
@@ -2,5 +2,5 @@ Rails.application.routes.draw do
   resources :react_posts
   resources :hybrid_posts
   resources :rails_posts
-  root 'rails_posts#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
+  root 'home#index'# For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
