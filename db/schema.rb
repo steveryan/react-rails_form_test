2c0045a06186b5d589fcb7aec380213b0e8eca46
steveryan <steveryan6989@gmail.com>
2019-06-25 20:40:01 -0400

    switches to postgres

--- db/schema.rb
+++ db/schema.rb
@@ -12,6 +12,9 @@
 
 ActiveRecord::Schema.define(version: 2019_06_25_234144) do
 
+  # These are extensions that must be enabled in order to support this database
+  enable_extension "plpgsql"
+
   create_table "hybrid_posts", force: :cascade do |t|
     t.string "title"
     t.string "content"
