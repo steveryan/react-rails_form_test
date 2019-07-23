5360a8397a1352552433230e315df278520537cd
steveryan <steveryan6989@gmail.com>
2019-07-22 20:53:35 -0400

    testing

--- scripts/test.rb
+++ scripts/test.rb
@@ -1,5 +1,6 @@
 response = `git diff --name-only --cached`
 list = response.split("\n")
 list.each do |file|
+  puts "minimizing #{file}" if File.extname(file) == ".svg"
   system("svgo #{file}") if File.extname(file) == ".svg"
 end
